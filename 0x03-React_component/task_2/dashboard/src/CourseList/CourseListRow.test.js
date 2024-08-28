fc7ff1fa8d90a1e4fb7e79bedd626d2856741859
valritehachi <valritehachi@gmail.com>
2024-08-28 19:50:10 +0000

    edits

---
+++ 0x03-React_component/task_2/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+
+describe("Course List Row component test", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseListRow textFirstCell="test" />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should render two cells when textSecondCell not null", () => {
+    const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test" />);
+
+    expect(wrapper.find("tr").children()).toHaveLength(2);
+    expect(wrapper.find("tr").childAt(0).html()).toEqual("<td>test</td>");
+    expect(wrapper.find("tr").childAt(1).html()).toEqual("<td>test</td>");
+  });
+});
